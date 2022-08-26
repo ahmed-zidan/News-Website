845a98ff78916bb72f6f139f81e91a0e55c7cdf0
ahmed zidan <zidanzezo17@gmail.com>
2022-08-26 14:24:21 +0200

    Add project files.

---
+++ News Website/Models/News.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace News_Website.Models
+{
+    public class News
+    {
+
+        public int Id { get; set; }
+        public string Titley { get; set; }
+        public DateTime Date { get; set; }
+        public string Image { get; set; }
+    }
+}
